dbc7733c7bdee4b90f4353d0d35bdc3b3ff20a0a
Mario Arro <marioarro186@gmail.com>
2021-12-17 12:42:06 +0200

    linkedin-clone app

---
+++ src/Post.js
@@ -0,0 +1,34 @@
+import { Avatar } from '@mui/material';
+import React, { forwardRef } from 'react';
+import InputOption from './InputOption';
+import './Post.css';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+
+const Post = forwardRef(({ name, description, message, photoUrl }, ref) => {
+  return (
+    <div ref={ref} className="post">
+      <div className="post__header">
+        <Avatar src={photoUrl}>{name[0]}</Avatar>
+        <div className="post__info">
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+      <div className="post__body">
+        <p>{message}</p>
+      </div>
+
+      <div className="post__buttons">
+        <InputOption Icon={ThumbUpOffAltIcon} title="Like" color="grey" />
+        <InputOption Icon={ChatOutlinedIcon} title="Comment" color="grey" />
+        <InputOption Icon={ShareOutlinedIcon} title="Share" color="grey" />
+        <InputOption Icon={SendOutlinedIcon} title="Send" color="grey" />
+      </div>
+    </div>
+  );
+});
+
+export default Post;
